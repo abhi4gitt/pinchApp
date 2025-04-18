1737c993037ea6d94eb5571b96f4001b0fd05cf8
abhi4gitt <raiabhishek631@gmail.com>
2025-04-18 22:16:48 +0530

    Control Interface

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,23 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Abhishek on 18/04/25.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    ControlImageView(icon: "minus.magnifyingglass")
+        .preferredColorScheme(.dark)
+        .padding()
+}
