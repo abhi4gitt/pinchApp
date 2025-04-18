64c4aa1f838056e0c17cc499b2974e6f723b5dea
abhi4gitt <raiabhishek631@gmail.com>
2025-04-18 15:40:43 +0530

    Pinch app setup

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Abhishek on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
